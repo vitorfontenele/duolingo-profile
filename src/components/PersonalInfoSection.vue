0d26a113906c03bf51c6771f2d0613f18659c872
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-12-31 00:38:34 -0300

    Change cards design

--- src/components/PersonalInfoSection.vue
+++ src/components/PersonalInfoSection.vue
@@ -2,9 +2,16 @@
     <section class="min-h-screen">
         <h2 class="text-center font-semibold mb-4 text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">Informações básicas</h2>
         <div class="flex flex-col gap-y-4 gap-x-4 sm:flex-row sm:justify-center">
-            <article class="flex flex-col items-center bg-teal-50 shadow gap-y-9 py-8 sm:rounded sm:w-80" v-for="user in usersData">
-                <div>
-                    <img :src="checkImage(user.picture)" alt="User photo" height="150" width="150" class="rounded-full">
+            <article class="relative flex flex-col items-center bg-neutral-50 shadow-xl gap-y-9 pt-8 sm:rounded sm:w-80" v-for="user in usersData">
+                <div class="sm:rounded-t absolute top-0 w-full bg-slate-700 h-[107px]"></div>
+                <div class="relative">
+                    <img 
+                        :src="checkImage(user.picture)" 
+                        :alt="`${user.name ? user.name : user.username}`" 
+                        height="150" 
+                        width="150" 
+                        class="shadow-[0px_3px_8px_rgba(0,0,0,0.24)] rounded-full"
+                    >
                 </div>
                 <div class="flex flex-col items-center">
                     <h3 class="font-semibold text-2xl text-center">{{ user.name ? user.name : user.username }}</h3>
@@ -16,35 +23,41 @@
                             alt="User country of origin"
                         >
                     </div>
-                    <a class="rounded shadow uppercase bg-teal-100 hover:bg-teal-200 px-5 py-1 mt-2" :href="`https://www.duolingo.com/profile/${user.username}`" target="_blank">Perfil</a>
                 </div>
-                <div class="w-full flex justify-around">
-                    <div>
-                        <div class="flex justify-center items-center gap-x-1">
-                            <div>
-                                <img 
-                                    src="https://d35aaqx5ub95lt.cloudfront.net/images/398e4298a3b39ce566050e5c041949ef.svg" 
-                                    alt="Fire for Duolingo streak"
-                                    class="h-5"
-                                >
+                <div class="w-full">
+                    <div class="flex">
+                        <div class="bg-slate-200 flex-grow py-4 border-t-2 border-r-2 border-slate-300">
+                            <div class="flex justify-center items-center gap-x-1">
+                                <div>
+                                    <img 
+                                        src="https://d35aaqx5ub95lt.cloudfront.net/images/398e4298a3b39ce566050e5c041949ef.svg" 
+                                        alt="Fire for Duolingo streak"
+                                        class="h-5"
+                                    >
+                                </div>
+                                <h4 class="font-semibold text-center">{{ user.streak }}</h4>
                             </div>
-                            <h4 class="font-semibold text-center">{{ user.streak }}</h4>
+                            <h5 class="font-light text-center">Dias seguidos</h5>
                         </div>
-                        <h5 class="font-light text-center">Dias seguidos</h5>
-                    </div>
-                    <div>
-                        <div class="flex justify-center items-center gap-x-1">
-                            <div>
-                                <img 
-                                    src="https://d35aaqx5ub95lt.cloudfront.net/images/profile/01ce3a817dd01842581c3d18debcbc46.svg" 
-                                    alt="Ray for Duolingo Total XP"
-                                    class="h-5"
-                                >
+                        <div class="bg-slate-200 flex-grow py-4 border-t-2 border-slate-300">
+                            <div class="flex justify-center items-center gap-x-1">
+                                <div>
+                                    <img 
+                                        src="https://d35aaqx5ub95lt.cloudfront.net/images/profile/01ce3a817dd01842581c3d18debcbc46.svg" 
+                                        alt="Ray for Duolingo Total XP"
+                                        class="h-5"
+                                    >
+                                </div>
+                                <h4 class="font-semibold text-center">{{ user.totalXp }}</h4>
                             </div>
-                            <h4 class="font-semibold text-center">{{ user.totalXp }}</h4>
+                            <h5 class="font-light text-center">Total XP</h5>
                         </div>
-                        <h5 class="font-light text-center">Total XP</h5>
                     </div>
+                    <a
+                        class="bg-green-600 hover:bg-green-700 text-neutral-50 text-lg text-center font-bold uppercase w-full block py-4 rounded-b"
+                        :href="`https://www.duolingo.com/profile/${user.username}`" 
+                        target="_blank"
+                    >Perfil</a>
                 </div>
             </article>
         </div>
